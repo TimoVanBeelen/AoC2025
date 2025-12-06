4036db93a5b8a88ef748daf3ddfae7af8c331194
s2237326 <t.vanbeelen@student.utwente.nl>
2025-12-06 15:51:56 +0100

    Deel 2 afgerond, was moeilijker dan verwacht en een beetje een gekke methode gebruikt

--- Day6/main.py
+++ Day6/main.py
@@ -4,7 +4,6 @@
 # Timo van Beelen
 
 
-import math
 import time
 
 
@@ -53,3 +52,48 @@ if __name__ == "__main__":
     end_pt1 = time.time()
     print("Time for part 1: ", (end_pt1-start_time)*1000, " ms")        # Runs: 6.5ms, 9.3ms, 5,9 ms
     
+    # Part 2: same story as part 1, but now after transposing, change the numbers and do not remove the ''
+
+    number = []
+    vals = []
+    for i in range(len(input_lines[0])-1):
+        if input_lines[-1][i+1] in ['+', '*']:
+            vals.append(number)
+            number = []
+        for j in range(len(input_lines)):
+            number.append(list(input_lines[j])[i])
+    for j in range(len(input_lines)):
+            number.append(list(input_lines[j])[-1])
+    vals.append(number)
+    # print(vals)
+
+    calcs = [''.join(item) for item in vals]
+    calcs = [s.replace('*', ' ') for s in calcs]
+    calcs = [s.replace('+', ' ') for s in calcs]
+
+    operators = [c for c in list(input_lines[-1]) if c != ' ']
+
+    calcs_split = [c.split(' ') for c in calcs]
+    for i in range(len(calcs_split)):
+        nums = [int(s.replace('', '')) for s in calcs_split[i] if s != '']
+        calcs_split[i] = nums
+        calcs_split[i].append(operators[i])
+
+    answers = []
+    for calc in calcs_split:
+        if calc[-1] == '+':
+            answers.append(sum([int(i) for i in calc[:-1]]))
+        elif calc[-1] == '-':
+            answers.append(sum(-1*int(i) for i in calc[:-1]))
+        elif calc[-1] == '*':
+            ans = 1
+            for i in calc[:-1]: ans *= int(i)
+            answers.append(ans)
+        else:
+            print("Unknown symbol: ", calc[-1])
+
+    print("Total sum of answers: ", sum(answers))
+
+    end_pt2 = time.time()
+    print("Time for part 2: ", (end_pt2-end_pt1)*1000, " ms")        # Runs: 251 ms, 242 ms, 277 ms
+        
